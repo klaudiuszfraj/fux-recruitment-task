8922286173040bced4ed475b4038acb661bebddb
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-04 10:52:36 +0200

    finish UserList display

--- src/screens/users/UsersList/UsersList.jsx
+++ src/screens/users/UsersList/UsersList.jsx
@@ -4,22 +4,31 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
+import {Box, Grid, Paper} from "@material-ui/core";
 
 const UsersList = () => {
   const users = useSelector(state => state.users.data);
-
+  console.log(users);
   return (
     <List>
-      {users.map(user => (
-        <ListItem key={`user-${user.id}`}>
-          <ListItemText
-            primary={
-              <Typography variant="h6">{`${user.first_name} ${user.last_name}`}</Typography>
-            }
-            secondary={user.email}
-          />
-        </ListItem>
-      ))}
+        <div style={{padding: '1rem'}}>
+          <Grid container direction={"column"} spacing={2}>
+            {users.map(user => (
+              <Grid item>
+                <Paper key={`user-${user.id}`} >
+                  <ListItem>
+                    <ListItemText
+                        primary={
+                          <Typography variant="h6">{`${user.first_name} ${user.last_name}`}</Typography>
+                        }
+                        secondary={user.email}
+                    />
+                  </ListItem>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
     </List>
   )
 };
