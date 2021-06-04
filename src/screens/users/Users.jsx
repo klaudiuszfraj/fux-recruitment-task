d9c9169e12486deacccf5c2116e52977bcd3b5e9
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-04 10:35:59 +0200

    add button for new users

--- src/screens/users/Users.jsx
+++ src/screens/users/Users.jsx
@@ -4,9 +4,12 @@ import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 import UsersList from './UsersList'
 import { loadUsers } from '../../redux/actions/usersActions';
+import {Grid} from "@material-ui/core";
 
 const Users = () => {
   const dispatch = useDispatch();
@@ -19,9 +22,17 @@ const Users = () => {
     <Container maxWidth="md">
       <Box mt={4} clone>
         <Paper >
-          <Box p={2} clone>
-            <Typography variant='h3'>Users</Typography>
-          </Box>
+          <div style={{ padding: 20 }}>
+            <Grid container justify={'space-between'} alignItems={'center'}>
+              <Box clone>
+                <Typography variant='h3' style={{fontSize: 24}}>Users</Typography>
+              </Box>
+              <Button variant="contained" style={{backgroundColor: '#43565f', color: '#fff'}} startIcon={<AddIcon/>}>
+                ADD USER
+              </Button>
+            </Grid>
+          </div>
+
           <UsersList />
         </Paper>
       </Box>
