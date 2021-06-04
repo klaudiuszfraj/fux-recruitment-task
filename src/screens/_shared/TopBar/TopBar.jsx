d6ae1430d6edd8995d4e7ede406678ce5d06912a
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-04 10:17:50 +0200

    TopBar done

--- src/screens/_shared/TopBar/TopBar.jsx
+++ src/screens/_shared/TopBar/TopBar.jsx
@@ -5,13 +5,14 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 
+//todo:: I cant download the icon from the mock up
 export default function TopBar() {
-
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{height: 60, backgroundColor: '#617D8B'}}>
       <Container maxWidth="md">
-        <Toolbar>
-          <Typography variant="h6" >
+        <Toolbar style={{padding: 0}}>
+            <div style={{width: 47, height: 45, backgroundColor: '#43565f', display: 'inline-block', marginRight: '1rem'}}/>
+            <Typography variant="h6" >
             App name
           </Typography>
         </Toolbar>
