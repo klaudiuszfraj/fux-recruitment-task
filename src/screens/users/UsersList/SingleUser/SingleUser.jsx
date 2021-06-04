6a741363f99a19b17e466a6ef0812c53990f8140
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-04 11:41:11 +0200

    display single user

--- src/screens/users/UsersList/SingleUser/SingleUser.jsx
+++ src/screens/users/UsersList/SingleUser/SingleUser.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import {Grid, Paper} from "@material-ui/core";
+import {ListItemAvatar, Paper, Avatar} from "@material-ui/core";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 
 const SingleUser = ({ user }) => {
+    console.log(user)
     return (
-        <Grid item>
             <Paper key={`user-${user.id}`} >
                 <ListItem>
+
                     <ListItemText
                         primary={
-                            <Typography variant="h6">{`${user.first_name} ${user.last_name}`}</Typography>
+                            <>
+                                <Typography variant="body2" style={{color: 'rgba(0,0,0,0.6)'}}>{`ID: ${user.id}`}</Typography>
+                                <Typography variant="h6" style={{fontSize: 24, fontWeight: 500}} >{`${user.first_name} ${user.last_name}`}</Typography>
+                            </>
                         }
                         secondary={user.email}
+
                     />
+                    <ListItemAvatar >
+                        <Avatar
+                            alt={`${user.first_name} ${user.last_name}`}
+                            src={user.avatar}
+                            variant={"square"}
+                            style={{minWidth: 103, minHeight: 84}}
+                        />
+                    </ListItemAvatar>
                 </ListItem>
             </Paper>
-        </Grid>
     );
 };
 
