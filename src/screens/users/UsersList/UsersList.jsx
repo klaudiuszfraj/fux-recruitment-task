8527b307e7472b8e1ec1cf3841a681ce55b7f1b8
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-05 16:13:41 +0200

    change import method

--- src/screens/users/UsersList/UsersList.jsx
+++ src/screens/users/UsersList/UsersList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import List from '@material-ui/core/List';
 import {Grid} from "@material-ui/core";
-import SingleUser from "./SingleUser/SingleUser";
+import SingleUser from "./SingleUser/";
 
 const UsersList = () => {
   const users = useSelector(state => state.users.data);
