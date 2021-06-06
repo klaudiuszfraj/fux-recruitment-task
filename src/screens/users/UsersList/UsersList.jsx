0d50a0b8a2480cb2f03fd3a37d2e3a21edad0719
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-06 11:31:49 +0200

    add display Alert functionality

--- src/screens/users/UsersList/UsersList.jsx
+++ src/screens/users/UsersList/UsersList.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector } from 'react-redux';
 import List from '@material-ui/core/List';
 import {Grid} from "@material-ui/core";
 import SingleUser from "./SingleUser/";
+import Alert from "../Alert";
+import {useLocation} from "react-router-dom";
+
 
 const UsersList = () => {
-  const users = useSelector(state => state.users.data);
-  return (
+    const [successMessage, setSuccessMessage] = useState('');
+    const [open, setOpen] = useState(false);
+    const location = useLocation();
+    const users = useSelector(state => state.users.data);
+
+    useEffect(() => {
+        if (location.state) {
+            setSuccessMessage(location.state.message);
+            setOpen(true)
+        }
+    },[location]);
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
     <List>
         <div style={{padding: '1rem'}}>
           <Grid container direction={"column"} spacing={2}>
@@ -17,6 +34,7 @@ const UsersList = () => {
             ))}
           </Grid>
         </div>
+        <Alert message={successMessage} open={open} onClose={handleClose}/>
     </List>
   )
 };
