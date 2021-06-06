5b1e86783279d4822f9d1a3d35f4d7b2dbf57a2b
Klaudiusz Frajkowski <klaudiusz.frajkowski@gmail.com>
2021-06-06 11:37:27 +0200

    add Logo

--- src/screens/_shared/TopBar/TopBar.jsx
+++ src/screens/_shared/TopBar/TopBar.jsx
@@ -3,15 +3,14 @@ import AppBar from '@material-ui/core/AppBar';
 import Container from '@material-ui/core/Container/Container';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import {ReactComponent as Logo} from '../../../assets/users-app-logo.svg';
 
-
-//todo:: I cant download the icon from the mock up
 export default function TopBar() {
   return (
     <AppBar position="static" style={{height: 60, backgroundColor: '#617D8B'}}>
       <Container maxWidth="md">
         <Toolbar style={{padding: 0}}>
-            <div style={{width: 47, height: 45, backgroundColor: '#43565f', display: 'inline-block', marginRight: '1rem'}}/>
+            <Logo style={{width: 47, height: 45, display: 'inline-block', marginRight: '1rem'}}/>
             <Typography variant="h6" >
                 Users app
           </Typography>
